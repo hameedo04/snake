9573e6e4d0d3faa869e052db0ae6b349c11e2fc5
muhammad reed <amdosm>
2022-10-17 09:38:46 -0400

    Initial Commit

---
+++ fiel open/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fiel open
+//
+//  Created by Muhammad Reed on 10/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
